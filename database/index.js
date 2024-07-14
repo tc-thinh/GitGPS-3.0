f1ecefb534747727e13b8f267d5e1b177be238f5
tcthinh <congthinh6304@gmail.com>
2024-07-14 13:04:33 -0700

    S-006: Database initialization

---
+++ database/index.js
@@ -0,0 +1,37 @@
+const sqlite3 = require('sqlite3');
+const sqlite = require('sqlite');
+const express = require('express');
+
+// TODO: Create a configuration object for the database
+const app = express(); // Initialize the Express application
+const port = 3000; // Set the server port to 3000
+const dbPath = "./instance/repos.sqlite";
+
+// Define the database connection, might be migrated to a separate module
+// Open a connection to the database and return a Promise
+const dbPromise = sqlite.open({
+    filename: dbPath,
+    driver: sqlite3.Database,
+});
+
+async function setupDB(args) {
+    const db = await dbPromise; // Await the resolved database promise to get the database connection
+    await db.migrate({
+        force: args.force,
+        // migrationsPath: "./migrations",
+    }); // Run the migration function to create the database tables
+}
+
+// Set up function
+async function setup() {
+    try {
+        await setupDB({ force: true }); // Set up the database from migrations
+        app.listen(port, () => {
+            console.log(`Server listening at http://localhost:${port}`); // Log the server's listening status
+        });
+    } catch (error) {
+        console.error(error); // Log any errors to the console
+    }
+}
+
+setup();
---
+++ database/migrations/001-initial-scheme.sql
@@ -0,0 +1,22 @@
+-- Up
+
+CREATE TABLE repositories (
+    repo_id INT AUTOINCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE repo_components (
+    component_id INT AUTOINCREMENT PRIMARY KEY,
+    repo_id INT NOT NULL FOREIGN KEY,
+    filename VARCHAR(255) NOT NULL,
+    filepath VARCHAR(1023) NOT NULL,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (repo_id) REFERENCES repositories(repo_id)
+);
+
+-- Down
+
+DROP TABLE repositories;
+DROP TABLE repo_components;
