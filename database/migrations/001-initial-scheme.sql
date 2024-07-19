43b10020d9c5e7148995a08d2ba6aaaa72f160b1
tcthinh <congthinh6304@gmail.com>
2024-07-18 20:17:27 -0700

    S-008 Internal SQL DB changes

--- database/migrations/001-initial-scheme.sql
+++ database/migrations/001-initial-scheme.sql
@@ -8,17 +8,33 @@ CREATE TABLE repositories (
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE repo_components (
-    component_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    repo_id INTEGER NOT NULL,
+CREATE TABLE repo_directories (
+    directory_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    repo_id INTEGER NULL,
+    parent_directory_id INTEGER NULL,
     filename VARCHAR(255) NOT NULL,
     filepath VARCHAR(1023) NOT NULL,
     description TEXT NULL,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (repo_id) REFERENCES repositories(repo_id)
+    FOREIGN KEY (repo_id) REFERENCES repositories(repo_id),
+    FOREIGN KEY (parent_directory_id) REFERENCES repo_directories(directory_id)
+);
+
+CREATE TABLE repo_files (
+    file_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    repo_id INTEGER NULL,
+    parent_directory_id INTEGER NULL,
+    filename VARCHAR(255) NOT NULL,
+    filepath VARCHAR(1023) NOT NULL,
+    description TEXT NULL,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    parent_directory_id INTEGER NOT NULL,
+    FOREIGN KEY (repo_id) REFERENCES repositories(repo_id),
+    FOREIGN KEY (parent_directory_id) REFERENCES repo_directories(directory_id)
 );
 
 -- Down
 
-DROP TABLE repo_components;
 DROP TABLE repositories;
+DROP TABLE repo_directories;
+DROP TABLE repo_files;
