e29c5f43a3ee8a8b1f6a614623d4f086e1495f50
tcthinh <congthinh6304@gmail.com>
2024-07-18 20:13:32 -0700

    S-008 Repo related endpoints

--- database/routes/repo.js
+++ database/routes/repo.js
@@ -2,20 +2,48 @@ require('module-alias/register'); // Import module-alias
 
 const express = require('express');
 const router = express.Router();
-const { insertRepository } = require('@models/repository');
+const { insertRepository, getRepository, searchRepository } = require('@models/repository');
 
 router.post("/add-repo", async (req, res) => {
-    const name = req.body.name;
-    const description = req.body.description;
-    const url = req.body.url; 
+    const { name, description, url } = req.body;
     try {
-        await insertRepository(name, description, url);
-    }
-    catch (error) {
+        const status = await insertRepository(name, description, url);
+        if (!status.ok) {
+            // Ensure response is sent once by returning here
+            return res.status(500).json(status); 
+        }
+        // 201 Created: Request succeeded, new resource created
+        res.status(201).json({ ok: true, description: "Repository added successfully." });  
+    } catch (error) {
         console.error(error);
-        return res.status(500).send('An error occurred while adding the repository');
+        return res.status(500).json({ ok: false, description: `Error: Internal server error occurred while inserting the new repo:\n${error}` });
     }
-    res.status(201).send('Repository added successfully');
 });
+
+router.get("/get-repo", async (req, res) => {
+    const { repo_id } = req.query;
+    try {
+        const repo = await getRepository(repo_id);
+        if (!repo) {
+            // 404 Not Found: Server cannot find the requested resource
+            return res.status(404).json({ ok: false, description: "Repository not found" });
+        }
+        return res.status(200).json({ ok: true, description: "Repository fetched successfully.", repo });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ ok: false, description: `Error: Internal server error occurred while fetching the repository with id=${repo_id}:\n${error}` });
+    }
+});
+
+router.get("/search-repo", async (req, res) => {
+    const { keyword } = req.query;
+    try {
+        const repos = await searchRepository(keyword);
+        return res.status(200).json(repos);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ ok: false, description: `Error: Internal server error occurred while fetching the repository:\n${error}` });
+    }
+})
 
 module.exports = router;
