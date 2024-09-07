4910c572d535bef71e37c785cb3b2c856f5fefdb
tcthinh <congthinh6304@gmail.com>
2024-09-06 23:07:24 -0700

    Load LLM model directly

--- backend/README.md
+++ backend/README.md
@@ -1,2 +1,3 @@
 Create `requirement.py` file: `pip freeze > requirements.txt`
 Install packages: `pip install -r requirements.txt`
+Run Python virtual environment: `.\.venv\Scripts\activate `
---
+++ backend/services/llm_service.py
@@ -0,0 +1,7 @@
+# Test: Load model directly
+
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-128k-instruct", trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained("microsoft/Phi-3-mini-128k-instruct", trust_remote_code=True)
